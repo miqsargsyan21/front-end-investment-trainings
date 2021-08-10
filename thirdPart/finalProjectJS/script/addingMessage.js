7ec2ac5af665a627a184c5f10b6332350d386432
Michael Sargsyan <37113476+miqsargsyan21@users.noreply.github.com>
2021-08-10 04:55:12 +0400

    Update addingMessage.js

--- thirdPart/finalProjectJS/script/addingMessage.js
+++ thirdPart/finalProjectJS/script/addingMessage.js
@@ -1,9 +1,14 @@
+function isValidEmail (email) {
+    let sample = /\S+@\S+\.\S+/;
+    return sample.test(email);
+}
+
 function addMessage () {
     let userName = document.getElementById('nameUser').value,
         userEmail = document.getElementById('emailUser').value,
         userMessage = document.getElementById('messageUser').value;
 
-    if (userEmail != "" && userEmail != " " && userName != "" && userName != " " && userMessage != "" && userMessage != " ") {
+    if (isValidEmail(userEmail) && userName != "" && userName != " " && userMessage != "" && userMessage != " ") {
         let newEmail = new Email(userName, userEmail, userMessage);
         emails.push(newEmail);
     } else {
